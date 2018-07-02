ecb480f69f47ea734eea62d150fc6a33c19e7056
Emlice <xiaomozyw@sina.com>
2018-07-02 11:34:40 +0800

    移动端 vw 布局启动方案

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/'
+import store from './store/'
+
+// 引入 Mint-ui
+import Mint from 'mint-ui';
+// join global style
+import './styles/index.scss'
+Vue.use(Mint);
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
     <div class="about">
         <h1>This is an about page</h1>
+        <div aspectratio emlice-top>
+            <div aspectratio-content>
+                <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1530276163439&di=c35165f55bd2322af59f6fdb114bb0a5&imgtype=0&src=http%3A%2F%2Fa.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Ffc1f4134970a304eb5088f73ddc8a786c9175c14.jpg" alt="">                
+            </div>
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+[emlice-top] {
+    width: 60vw;
+    margin: 10px auto
+}
+[emlice-top] {
+    aspect-ratio: '1000:1500'
+}
+img { width: 100% }
+</style>
