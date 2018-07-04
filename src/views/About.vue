e5d419af3b7cfce4f85f03900d0c04ad4e17daa0
Emlice <xiaomozyw@sina.com>
2018-07-04 15:33:00 +0800

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,19 @@
+<script>
+export default {
+    data () {
+        return {
+            imgUrl: `https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1530276163439&di=c35165f55bd2322af59f6fdb114bb0a5&imgtype=0&src=http%3A%2F%2Fa.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Ffc1f4134970a304eb5088f73ddc8a786c9175c14.jpg`
+        }
+    }
+}
+</script>
+
 <template>
     <div class="about">
         <h1>This is an about page</h1>
         <div aspectratio emlice-top>
             <div aspectratio-content>
-                <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1530276163439&di=c35165f55bd2322af59f6fdb114bb0a5&imgtype=0&src=http%3A%2F%2Fa.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Ffc1f4134970a304eb5088f73ddc8a786c9175c14.jpg" alt="">                
+                <img :src="imgUrl" alt="">                
             </div>
         </div>
     </div>
